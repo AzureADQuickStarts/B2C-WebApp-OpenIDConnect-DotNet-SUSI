0098548eec35f14fc8cf7d7e4ac97ac6c9bc8227
dstrockis <dastrock@microsoft.com>
2016-04-07 05:58:57 -0700

    Initial Commit

---
+++ WebApp-B2C-DotNet/App_Start/Startup.Auth.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+// The following using statements were added for this sample
+using Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Threading.Tasks;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.IdentityModel.Protocols;
+using System.Web.Mvc;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+using WebApp_OpenIDConnect_DotNet_B2C.Policies;
+using System.Threading;
+using System.Globalization;
+
+namespace WebApp_OpenIDConnect_DotNet_B2C
+{
+	public partial class Startup
+	{
+        // The ACR claim is used to indicate which policy was executed
+        public const string AcrClaimType = "http://schemas.microsoft.com/claims/authnclassreference";
+        public const string PolicyKey = "b2cpolicy";
+        public const string OIDCMetadataSuffix = "/.well-known/openid-configuration";
+
+        // App config settings
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AadInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string redirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"];
+
+        // B2C policy identifiers
+        public static string SusiPolicyId = ConfigurationManager.AppSettings["ida:SusiPolicyId"];
+        public static string PasswordResetPolicyId = ConfigurationManager.AppSettings["ida:PasswordResetPolicyId"];
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            OpenIdConnectAuthenticationOptions options = new OpenIdConnectAuthenticationOptions
+            {
+                // These are standard OpenID Connect parameters, with values pulled from web.config
+                ClientId = clientId,
+                RedirectUri = redirectUri,
+                PostLogoutRedirectUri = redirectUri,
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                { 
+                    AuthenticationFailed = AuthenticationFailed,
+                    RedirectToIdentityProvider = OnRedirectToIdentityProvider,
+                    SecurityTokenValidated = OnSecurityTokenValidated,
+                },
+                Scope = "openid",
+                ResponseType = "id_token",
+
+                // The PolicyConfigurationManager takes care of getting the correct Azure AD authentication
+                // endpoints from the OpenID Connect metadata endpoint.  It is included in the PolicyAuthHelpers folder.
+                ConfigurationManager = new PolicyConfigurationManager(
+                    String.Format(CultureInfo.InvariantCulture, aadInstance, tenant, "/v2.0", OIDCMetadataSuffix),
+                    new string[] { SusiPolicyId, PasswordResetPolicyId }),
+
+                // This piece is optional - it is used for displaying the user's name in the navigation bar.
+                TokenValidationParameters = new TokenValidationParameters
+                {  
+                    NameClaimType = "name",
+                },
+            };
+
+            app.UseOpenIdConnectAuthentication(options);
+                
+        }
+
+        // This notification can be used to manipulate the OIDC request before it is sent.  Here we use it to send the correct policy.
+        private async Task OnRedirectToIdentityProvider(RedirectToIdentityProviderNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
+        {
+            PolicyConfigurationManager mgr = notification.Options.ConfigurationManager as PolicyConfigurationManager;
+            if (notification.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
+            {
+                OpenIdConnectConfiguration config = await mgr.GetConfigurationByPolicyAsync(CancellationToken.None, notification.OwinContext.Authentication.AuthenticationResponseRevoke.Properties.Dictionary[Startup.PolicyKey]);
+                notification.ProtocolMessage.IssuerAddress = config.EndSessionEndpoint;
+            }
+            else
+            {
+                OpenIdConnectConfiguration config = await mgr.GetConfigurationByPolicyAsync(CancellationToken.None, notification.OwinContext.Authentication.AuthenticationResponseChallenge.Properties.Dictionary[Startup.PolicyKey]);
+                notification.ProtocolMessage.IssuerAddress = config.AuthorizationEndpoint;
+            }
+        }
+
+        // Used for avoiding yellow-screen-of-death TODO
+        private Task AuthenticationFailed(AuthenticationFailedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
+        {
+            notification.HandleResponse();
+
+            if (notification.ProtocolMessage.ErrorDescription != null && notification.ProtocolMessage.ErrorDescription.Contains("AADB2C90091"))
+            {
+                notification.Response.Redirect("/Account/ResetPassword");
+            }
+            else
+            {
+                notification.Response.Redirect("/Home/Error?message=" + notification.Exception.Message);
+            }
+
+            return Task.FromResult(0);
+        }
+
+        private Task OnSecurityTokenValidated(SecurityTokenValidatedNotification<OpenIdConnectMessage, OpenIdConnectAuthenticationOptions> notification)
+        {
+            // If you wanted to keep some local state in the app (like a db of signed up users),
+            // you could use this notification to create the user record if it does not already
+            // exist.
+
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ WebApp-B2C-DotNet/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+// The following using statements were added for this sample.
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security.Cookies;
+using WebApp_OpenIDConnect_DotNet_B2C.Policies;
+using System.Security.Claims;
+
+namespace WebApp_OpenIDConnect_DotNet_B2C.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void Login()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                // To execute a policy, you simply need to trigger an OWIN challenge.
+                // You can indicate which policy to use by adding it to the AuthenticationProperties using the PolicyKey provided.
+
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties (
+                        new Dictionary<string, string> 
+                        { 
+                            {Startup.PolicyKey, Startup.SusiPolicyId}
+                        })
+                    { 
+                        RedirectUri = "/", 
+                    }, OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        public void ResetPassword()
+        {
+            HttpContext.GetOwinContext().Authentication.Challenge(
+                new AuthenticationProperties(
+                    new Dictionary<string, string>
+                    {
+                        {Startup.PolicyKey, Startup.PasswordResetPolicyId}
+                    })
+                {
+                    RedirectUri = "/",
+                }, OpenIdConnectAuthenticationDefaults.AuthenticationType);
+        }
+
+        public void Logout()
+        {
+            // To sign out the user, you should issue an OpenIDConnect sign out request using the last policy that the user executed.
+            // This is as easy as looking up the current value of the ACR claim, adding it to the AuthenticationProperties, and making an OWIN SignOut call.
+
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                new AuthenticationProperties(
+                    new Dictionary<string, string> 
+                    { 
+                        {Startup.PolicyKey, ClaimsPrincipal.Current.FindFirst(Startup.AcrClaimType).Value}
+                    }), OpenIdConnectAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType);
+        }
+	}
+}
